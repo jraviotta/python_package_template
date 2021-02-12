2472cf8405b071d384a0612a2c393fc63404999c
Jonathan Raviotta <jraviotta@gmail.com>
2021-02-12 10:56:31 -0500

    removed duplicate packages from setup

--- setup.py
+++ setup.py
@@ -8,9 +8,9 @@ setup(
     author='jraviotta',
     license='MIT',
     install_requires=[
-        'Click', 'O365', 'python-dotenv', 'numpy', 'pandas>=1', 'xlrd',
-        'flask', 'flake8', 'ipykernel', 'python-dotenv', 'termcolor', 'PyCap',
-        'flask', 'plotly', 'nbformat'
+        'Click', 'O365', 'python-dotenv', 'numpy', 'pandas>=1', 'flask',
+        'flake8', 'ipykernel', 'seaborn', 'termcolor', 'PyCap', 'plotly',
+        'nbformat'
     ],
     dependency_links=[
         'http://github.com/redcap-tools/PyCap/tarball/master#egg=package-1.0'
