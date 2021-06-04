920efdfd73b7ab9c98d0fda2975242b03901ead8
Jonathan Raviotta <jraviotta@gmail.com>
2021-06-04 16:51:02 -0400

    update to setup

--- setup.py
+++ setup.py
@@ -2,29 +2,29 @@ from setuptools import find_packages, setup
 
 setup(
     name="package_template",  # update
+    version="0.1.0",  # update
+    description="",  # update
+    author="jraviotta",  # update
+    license="MIT",  # update
     packages=find_packages(),
-    version="0.1.0",
-    description="",
-    author="jraviotta",
-    license="MIT",
     install_requires=[
         "Click",
+        "setuptools",
         "python-dotenv",
-        "numpy",
-        "pandas>=1",
         "flake8",
         "black",
         "ipykernel",
         "termcolor",
         "nbformat",
-        "seaborn",
         "nbconvert",
         "jupyter_contrib_nbextensions",
-        "sklearn",
+        "numpy",
+        "pandas>=1",
+        "seaborn",
     ],
     dependency_links=[],
-    entry_points="""
-        [console_scripts]
-        package_template=src.cli:cli 
-    """,  # update
+    entry_points=dict(
+        console_scripts=[
+            'package_template=src.cli:cli']  #update
+        )
 )
--- src/config.py
+++ src/config.py
@@ -13,55 +13,55 @@ datetime_is_numeric = True
 
 # place sensitive global variables in .env or ~/.credentials/.env
 # Read .env files
-files = [find_dotenv(), Path(Path.home() / ".credentials", '.env')]
+files = [find_dotenv(), Path(Path.home() / ".credentials", ".env")]
 for f in files:
     load_dotenv(f)
-    logger.info(f'loading: {f}')
-
+    logger.info(f"loading: {f}")
+credentialsPath = Path(Path.home(), ".credentials")
 # Set some paths
 projectRoot = Path(__file__).parent.parent.absolute()
 
 logger.info(f"Changing dir to {projectRoot}")
 os.chdir(projectRoot)
 
-rawData = os.environ.get('RAW_DATA')
+rawData = os.environ.get("RAW_DATA")
 if not rawData:
-    rawData = Path(projectRoot / 'data' / 'raw')
+    rawData = Path(projectRoot / "data" / "raw")
     if not Path.exists(rawData):
         os.mkdir(rawData)
 
-processedData = os.environ.get('PROCESSED_DATA')
+processedData = os.environ.get("PROCESSED_DATA")
 if not processedData:
-    processedData = Path(projectRoot / 'data' / 'processed')
+    processedData = Path(projectRoot / "data" / "processed")
     if not Path.exists(processedData):
         os.mkdir(processedData)
 
-documents = os.environ.get('DOCUMENTS')
+documents = os.environ.get("DOCUMENTS")
 if not documents:
     if documents is None:
-        documents = Path(projectRoot / 'documents')
+        documents = Path(projectRoot / "documents")
         if not Path.exists(documents):
             os.mkdir(documents)
 
-figures = os.environ.get('FIGURES')
+figures = os.environ.get("FIGURES")
 if not figures:
     if figures is None:
-        figures = Path(projectRoot / 'figures')
+        figures = Path(projectRoot / "figures")
         if not Path.exists(figures):
             os.mkdir(figures)
 
 # Configure logging
-VERBOSITY='INFO'
+VERBOSITY = "INFO"
 noisyLibs = [
-    'googleapiclient.discovery',
-    'requests_oauthlib.oauth2_session',
-    'matplotlib.font_manager',
-    'urllib3.connectionpool',
+    "googleapiclient.discovery",
+    "requests_oauthlib.oauth2_session",
+    "matplotlib.font_manager",
+    "urllib3.connectionpool",
 ]
 
 # Configure pandas
 try:
-    logger.info('Setting pandas display options')
+    logger.info("Setting pandas display options")
     pd.options.display.width = None
     pd.options.display.max_rows = 200
     pd.options.display.min_rows = 100
@@ -74,7 +74,24 @@ except ModuleNotFoundError:
 # Configure matplotlib
 try:
     import matplotlib
-    logger.info('Setting matplotlib display options')
+
+    logger.info("Setting matplotlib display options")
     # matplotlib.use('Agg')
 except Exception as e:
     print(e)
+
+# Configure SAS
+SAS_config_names = ["oda"]
+oda = {
+    "java": "/usr/bin/java",
+    # US Home Region
+    "iomhost": [
+        "odaws01-usw2.oda.sas.com",
+        "odaws02-usw2.oda.sas.com",
+        "odaws03-usw2.oda.sas.com",
+        "odaws04-usw2.oda.sas.com",
+    ],
+    "iomport": 8591,
+    "authkey": "oda",
+    "encoding": "utf-8",
+}
